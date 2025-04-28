d53611f077c57a11418f368ec80be929e11c4384
Suzuki <26lpc122002@gmail.com>
2025-04-29 03:19:05 +0700

    Homework 3

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*/*.html"
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        h2: ["48px", "64px"],
+      },
+    }
+  },
+  plugins: [],
+}
